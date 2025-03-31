0157c4f7c6bc0b5579b8260a21d247c62d326261
Shvan <shvan.yakana@student.hu.nl>
2025-03-31 09:25:39 +0200

    feat(ui): restyle calculator and update HTML layout
    Refactored the calculator's styling for a modern
    look, adjusting colors and buttons design.
    updated the html structure to improve layout
    for using CSS grid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,38 @@
 console.log("hello world");
+const content = document.querySelector(".content");
 
-numOne = Number(prompt("first number"));
-numTwo = Number(prompt("second number"));
+// const main = () => {
+//   const numOne = Number(prompt("first number"));
+//   const operator = prompt("choose your operator");
+//   const numTwo = Number(prompt("second number"));
+//   operate(numOne, operator, numTwo);
+// };
 
-const addition = (numOne, numTwo) => {
-  let result;
-  result = numOne + numTwo;
-  return result;
+const operate = (numOne, operator, numTwo) => {
+  switch (operator) {
+    case "+":
+      console.log(addition(numOne, numTwo));
+      break;
+    case "-":
+      console.log(substraction(numOne, numTwo));
+      break;
+    case "*":
+      multiplier(numOne, numTwo);
+      break;
+    case "/":
+      division(numOne, numTwo);
+      break;
+    default:
+      console.log("oops something went wrong");
+  }
 };
 
-alert(addition(numOne, numTwo));
+const addition = (numOne, numTwo) => numOne + numTwo;
+
+const substraction = (numOne, numTwo) => numOne - numTwo;
+
+const multiplier = (numOne, numTwo) => numOne * numTwo;
+
+const division = (numOne, numTwo) => numOne / numTwo;
+
+// main();
